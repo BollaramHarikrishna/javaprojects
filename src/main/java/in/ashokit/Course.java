39cf2f9ac8df83328c2285778b783697e600946c
BollaramHarikrishna <129168720+BollaramHarikrishna@users.noreply.github.com>
2023-03-28 20:34:50 +0530

    file added

--- src/main/java/in/ashokit/Course.java
+++ src/main/java/in/ashokit/Course.java
@@ -15,5 +15,23 @@ public class Course {
 	private Integer cid;
 	private String name;
 	private Double price;
+	public Integer getCid() {
+		return cid;
+	}
+	public void setCid(Integer cid) {
+		this.cid = cid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
 }
